4e3c6a844adfed80b786fe378248a0a695e0dfff
Alex Lawrence <35901757+UkraineBolt@users.noreply.github.com>
2018-08-10 02:09:18 -0400

    Add files via upload

--- OddOrEven/src/BaseCode/Main.java
+++ OddOrEven/src/BaseCode/Main.java
@@ -6,28 +6,34 @@ public class Main {
 
     public static void main(String[] args) {
         OddOrEvenSolver solver = new OddOrEvenSolver();
+        Scanner numberOrRange = new Scanner(System.in);
         System.out.println("a test to see if im able to determine if a whole number is even or odd without the use of bits");
         System.out.println("enter any number or range of numbers (ex: 20->50)");
         System.out.println("the range option will apply the test to multiple numbers for testing");
-        Scanner numberOrRange = new Scanner(System.in);
-        String stringNumber = numberOrRange.nextLine();
-        String[] splitInput = stringNumber.split("->");
-        if(splitInput.length==1){
-            if(solver.evenSolver(Integer.parseInt(splitInput[0]))){
-                System.out.println("Even");
-            }else {
-                System.out.println("Odd");
+        while(true){
+            System.out.println("\nenter any number or range of numbers (ex: 20->50)");
+            String stringNumber = numberOrRange.nextLine();
+            String[] splitInput = stringNumber.split("->");
+            if(splitInput[0].trim().toLowerCase().equals("quit")){
+                break;
             }
-        }else if(splitInput.length==2){
-            int startOfRange = Integer.parseInt(splitInput[0]);
-            int endOfRange = Integer.parseInt(splitInput[1]);
-            for(int i=startOfRange;i<=endOfRange;i++){
-                if(solver.evenSolver(i)){
-                    System.out.println(i+"is Even");
-                }else{
-                    System.out.println(i+"is Odd");
+            if(splitInput.length==1){
+                if(solver.evenSolver(Integer.parseInt(splitInput[0]))){
+                    System.out.println("Even");
+                }else {
+                    System.out.println("Odd");
                 }
+            }else if(splitInput.length==2){
+                int startOfRange = Integer.parseInt(splitInput[0].trim());
+                int endOfRange = Integer.parseInt(splitInput[1].trim());
+                for(int i=startOfRange;i<=endOfRange;i++){
+                    if(solver.evenSolver(i)){
+                        System.out.println(i+" is Even");
+                    }else{
+                        System.out.println(i+" is Odd");
+                    }
 
+                }
             }
         }
     }
